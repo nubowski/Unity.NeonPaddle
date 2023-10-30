7659d64572bdbe96cc78e2969a94fbaa94332634
nubowski <nubowski@nnteam.ru>
2023-10-30 20:08:13 +0300

    feat: add Gameinit GO and script

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+        [SerializeField] private GameObject uiLoaderPrefab;
+
+        private void Start()
+        {
+                InitializeSystems();
+                InitializeUI();
+        }
+
+        private void InitializeSystems()
+        {
+                // for future stuff for ini/upload/bundle/whatever
+        }
+
+        private void InitializeUI()
+        {
+             // simple UI starter
+             Instantiate(uiLoaderPrefab);
+        }
+}
